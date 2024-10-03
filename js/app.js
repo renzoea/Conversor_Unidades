41ed4819b2c398f4c5739ffe406ad50f5ce575be
raez02 <aezequiel449@gmail.com>
2024-10-02 21:01:06 -0300

    convertodo 1.2

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let temperatura = document.getElementById("temperatura");
+let energia = document.getElementById("energia");
+let longitud = document.getElementById("longitud");
+let frecuencia = document.getElementById("frecuencia");
+let tiempo = document.getElementById("tiempo");
+let slect1 = document.getElementById("select1");
+let slect2 = document.getElementById("select2");

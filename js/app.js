250b70eba2b0f1249e03b7ae22bc78057d115552
renzoea <aezequiel449@gmail.com>
2024-10-08 16:31:47 -0300

    pasaje unidades 1.0

--- js/app.js
+++ js/app.js
@@ -5,11 +5,14 @@ let frecuencia = document.getElementById("frecuencia");
 let tiempo = document.getElementById("tiempo");
 let select1 = document.getElementById("select1");
 let select2 = document.getElementById("select2");
+let inp1 = document.getElementById("inp1");
+let inp2 = document.getElementById("inp2");
 let tmp = ["celcius","Kelvin","Fahrenheit",];
 let ener = ["kilo Whats","Joule","Kilo Joule","Caloria-gramo","Kilo Caloria","Volt Hora","Kilo Volt Hora"]
 let frec = ["Herz","Kiloherz","Megaherz","Gigaherz"]
 let lon = ["Metro","Kilometro","Cetrimetro","Milimitro","Milla","Yarda","Pie","Pulgada"]
 let timp = ["Micro segundo","Mili Segundo","Segundo", "Minuto", "Hora", "Dia", "Semana","Mes","Año"]
+
 
 temperatura.addEventListener("click", function(){
     select1.innerHTML = '<option value="">Seleccione una unidad</option>';
@@ -23,9 +26,84 @@ temperatura.addEventListener("click", function(){
         let dat = document.createElement('option');
         dat.value = tmp[x];
         dat.textContent = tmp[x];
-        select2.appendChild(dat)
+        select2.appendChild(dat);
     }
-})
+    // if ((select1.value='celcius') && (select2.value='Kelvin')) {
+    //     inp1.onkeypress = function(){
+    //         let num=0;
+    //         num=inp1 + 273.15;
+    //         inp2.value= isNaN(num);
+    //         inp2.innerHTML=num;
+    //     }
+    // }
+});
+inp1.addEventListener('input', function() {
+    if (select1.value === 'celcius' && select2.value === 'Kelvin' ) {
+        const celsius = parseFloat(inp1.value);
+        if (!isNaN(celsius)) {
+            const kelvin = celsius + 273.15;
+            inp2.value = kelvin.toFixed(2); 
+        } else {
+            inp2.value = ''; 
+        }
+    }else if (select1.value === 'celcius' && select2.value === 'Fahrenheit' ) {
+        const celsius = parseFloat(inp1.value);
+        if (!isNaN(celsius)) {
+            const kelvin = (celsius / 1.8)+32;
+            inp2.value = kelvin.toFixed(2); 
+        } else {
+            inp2.value = ''; 
+        }
+    }else if(select1.value == 'celcius' && select2.value == 'celcius'){
+        const celsius = parseFloat(inp1.value);
+        if (!isNaN(celsius)) {
+            const kelvin = celsius;
+            inp2.value = kelvin.toFixed(2); 
+            console.log(kelvin);    
+        } else {
+            inp2.value = ''; 
+        }
+    }else if (select1.value === 'Kelvin' && select2.value === 'celcius' ) {
+        const celsius = parseFloat(inp1.value);
+        if (!isNaN(celsius)) {
+            const kelvin = celsius - 273.15;
+            inp2.value = kelvin.toFixed(2); 
+        } else {
+            inp2.value = ''; 
+        }
+    }else if (select1.value === 'Kelvin' && select2.value === 'Fahrenheit' ) {
+        const celsius = parseFloat(inp1.value);
+        if (!isNaN(celsius)) {
+            const kelvin = (((celsius + 273.15)*1.8)+32);
+            inp2.value = kelvin.toFixed(2); 
+        } else {
+            inp2.value = ''; 
+        }
+    }else if (select1.value === 'Kelvin' && select2.value === 'Kelvin' ) {
+        const celsius = parseFloat(inp1.value);
+        if (!isNaN(celsius)) {
+            const kelvin = celsius;
+            inp2.value = kelvin.toFixed(2); 
+        } else {
+            inp2.value = ''; 
+        }}else if (select1.value === 'Fahrenheit' && select2.value === 'celcius' ) {
+            const celsius = parseFloat(inp1.value);
+            if (!isNaN(celsius)) {
+                const kelvin = ((celsius -32)*0.56);
+                inp2.value = kelvin.toFixed(2); 
+            } else {
+                inp2.value = ''; 
+            }
+        }else if (select1.value === 'Fahrenheit' && select2.value === 'Kelvin' ) {
+            const celsius = parseFloat(inp1.value);
+            if (!isNaN(celsius)) {
+                const kelvin = ((celsius -32)*0.56)+273.15;
+                inp2.value = kelvin.toFixed(2); 
+            } else {
+                inp2.value = ''; 
+            }
+        }
+    });
 
 energia.addEventListener("click", function(){
     select1.innerHTML = '<option value="">Seleccione una unidad</option>';
@@ -39,9 +117,9 @@ energia.addEventListener("click", function(){
         let dat = document.createElement('option');
         dat.value = ener[x];
         dat.textContent = ener[x];
-        select2.appendChild(dat)
+        select2.appendChild(dat);
     }
-})
+});
 
 frecuencia.addEventListener("click", function(){
     select1.innerHTML = '<option value="">Seleccione una unidad</option>';
@@ -55,9 +133,9 @@ frecuencia.addEventListener("click", function(){
         let dat = document.createElement('option');
         dat.value = tmp[x];
         dat.textContent = tmp[x];
-        select2.appendChild(dat)
+        select2.appendChild(dat);
     }
-})
+});
 
 tiempo.addEventListener("click", function(){
     select1.innerHTML = '<option value="">Seleccione una unidad</option>';
@@ -71,9 +149,9 @@ tiempo.addEventListener("click", function(){
         let dat = document.createElement('option');
         dat.value = timp[x];
         dat.textContent = timp[x];
-        select2.appendChild(dat)
+        select2.appendChild(dat);
     }
-})
+});
 
 longitud.addEventListener("click", function(){
     select1.innerHTML = '<option value="">Seleccione una unidad</option>';
@@ -87,7 +165,7 @@ longitud.addEventListener("click", function(){
         let dat = document.createElement('option');
         dat.value = lon[x];
         dat.textContent = lon[x];
-        select2.appendChild(dat)
+        select2.appendChild(dat);
     }
     
-})
+});

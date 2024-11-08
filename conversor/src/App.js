76e66a2309c93067ce23f9cff29e8b3c3130e559
pablogiampe <pablogiampe@gmail.com>
2024-11-08 15:26:46 -0300

    cambios

--- conversor/src/App.js
+++ conversor/src/App.js
@@ -1,32 +1,41 @@
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Conversor from "./components/Conversor";
 
-import './App.css';
-import AcercaDe from './components/acercade';
+import "./App.css";
+import AcercaDe from "./components/acercade";
 function App() {
   return (
     <Router>
-       <nav>
-        <figure>
-          <a href="index.html">
-            <img class="logo" id="logo" src="img/logo.png" alt="Logo"
-          /></a>
-        </figure>
-        
-        <button class="abrir-btn" id="botonera">
-   Menu
-        </button> 
-        <ul class="lista" id="lista">
-          <button class="cerrar-btn" id="cerrar">Cerrar</button>
-          <li><a href="index.html">Inicio</a></li>
-          <li><Link to='/acercade'>Acerca de</Link></li>
-         
-        </ul>
-      </nav>
-      <Routes>
-        <Route path='/acercade' element={<AcercaDe/>}/>
-        </Routes>
-      </Router>
+      <div className="App">
+        <nav>
+          <figure>
+            <a href="index.html">
+              <img class="logo" id="logo" src="img/logo.png" alt="Logo" />
+            </a>
+          </figure>
 
+          <button class="abrir-btn" id="botonera">
+            Menu
+          </button>
+          <ul class="lista" id="lista">
+            <button class="cerrar-btn" id="cerrar">
+              Cerrar
+            </button>
+            <li>
+            <Link to='/' >Inicio</Link>
+            </li>
+            <li>
+              <Link to="/acercade">Acerca de</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Conversor />} />
+        <Route path="/acercade" element={<AcercaDe />} />
+      </Routes>
+    </Router>
   );
 }
 
--- conversor/src/components/Conversor.js
+++ conversor/src/components/Conversor.js
@@ -1,1 +1,51 @@
-import React from 'react';
+import React from 'react'
+
+const Conversor = () => {
+  return (
+    <main class="parent">
+    <div class="div1 botones">
+      <button class="btnSelect" id="temperatura">Temperatura</button>
+      <button class="btnSelect" id="frecuencia">Frecuencia</button>
+      <button class="btnSelect" id="longitud">Longitud</button>
+      <button class="btnSelect" id="tiempo">Tiempo</button>
+      <button class="btnSelect" id="energia">Energia</button>
+    </div>
+    <div class="div2"></div>
+
+    <div class="div3"><hr class="hrs"/>
+      <div class="conversor">
+      <div class="select">
+        <input type="number" id="inp1" />
+        <select name="Selecion" id="select1">
+          <option value="">Seleccione una unidad</option>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+        </select>
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 -960 960 960"
+        width="28px"
+        fill="#000000"
+      >
+        <path d="M160-280v-120h640v120H160Zm0-280v-120h640v120H160Z" />
+      </svg>
+      <div class="select">
+        <input type="number" id="inp2" />
+        <select name="Selecion1" id="select2">
+          <option value="">Seleccione una unidad</option>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+        </select>
+      </div></div><hr class="hrs"/>
+    </div>
+    
+    <div class="div4"></div>
+  </main>
+  )
+}
+
+export default Conversor
+
